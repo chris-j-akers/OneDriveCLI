76e2e1c862642c6786241a55fcde0f7562132696
Chris Akers <chris.j.akers@gmail.com>
2024-03-03 12:17:59 +0000

    Update

--- main.py
+++ main.py
@@ -9,32 +9,31 @@ import base64
 # App/Client ID: 9806a116-6f7d-4154-a06e-0c887dd51eed
 # Tenant ID: 42a7cc42-d023-4e93-898d-3777ba423ebe
 
+ONE_DRIVE_ENDPOINT = 'https://graph.microsoft.com/v1.0/me/drive'
+  
 
-def main():
+def delta_query(token):
+    api_headers = {
+    "Authorization": f"bearer {token}",
+    "Accept": "application/json"
+    }
+    response = requests.get(ONE_DRIVE_ENDPOINT + '/root/delta', headers=api_headers)
+    json_response = json.dumps(response.json(), indent=2)
+    print(json_response)  
 
+
+def get_token():
     logging.basicConfig(level=logging.ERROR)
     token_handler = MSALTokenHandler('OneDriveSync',
-                                client_id='9806a116-6f7d-4154-a06e-0c887dd51eed', 
-                                authority='https://login.microsoftonline.com/consumers',
-                                scopes=['Files.Read', 'User.Read'],
-                                db_filepath='./accounts.db')
-   
-    GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0'
-    ONE_DRIVE = '/me/drive'
-    
-    api_headers = {
-        "Authorization": f"bearer {token_handler.get_token()}",
-        "Accept": "application/json"
-    }
-    response = requests.get(GRAPH_ENDPOINT + ONE_DRIVE, headers=api_headers)
-    json_response = json.dumps(response.json(), indent=2)
-    print(json_response)
-    
-    drive_id = response.json()['id']
-    response = requests.get(GRAPH_ENDPOINT + f"/drives/{drive_id}/root/children", headers=api_headers)
+                            client_id='9806a116-6f7d-4154-a06e-0c887dd51eed', 
+                            authority='https://login.microsoftonline.com/consumers',
+                            scopes=['Files.Read', 'User.Read'],
+                            db_filepath='./accounts.db')
+    return token_handler.get_token()
 
-    json_response = json.dumps(response.json(), indent=2)
-    print(json_response)
+def main():
+    token = get_token()
+    delta_query(token)
 
 if __name__ == '__main__':
     main()
