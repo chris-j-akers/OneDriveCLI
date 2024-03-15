acc4ca060e1fa3d2cedc070b48452f28e52f1c35
Chris Akers <chris.j.akers@gmail.com>
2024-03-15 11:03:36 +0000

    refactor

--- TinyAcceptorServer.py
+++ TinyAcceptorServer.py
@@ -18,8 +18,11 @@ class TinyAcceptorServer(HTTPServer):
 
     NOTE: This is a very basic, simple HTTP server and, though, it doesn't serve
     any files or local directories it's still presents an attack surface that could
-    be open for as long as the timeout you have selected. Firewalls etc. should
-    mitigate, but it is up to you whether to use it or not.
+    be open for as long as the timeout you have selected.
+    
+    Firewalls etc. should mitigate and it lets your OS pick the port. We also 
+    check the state value returned with the authorisation code is the one that 
+    we sent. Ultimately, up to you whether to use it or not.
     """
     class Handler(SimpleHTTPRequestHandler):
         """
@@ -34,45 +37,57 @@ class TinyAcceptorServer(HTTPServer):
         def do_GET(self):
             data = parse_qs(self.path[2:])
             code = data.get('code','')
+            state = data.get('state', '')
+            error = data.get('error','')
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
+            if error != '':
+                print(f'error: got error in MSFT response: {error}.')
+                return ''                
+            if self.server.get_state() != state[0]:
+                print(f'error: state returned from MSFT does not match state sent in authorisation request (sent: {state[0]}, recvd: {self.server.get_state()}).')
+                return ''
             if code == '':
-                self.wfile.write(bytes('error: unable to get authorisation code from MSFT', 'utf8'))
-                error = data.get('error', 'no error text found')
-                self.server.set_error(error)
+                self.wfile.write(bytes('error: didn\'t seem to get authorisation code from MSFT, and no error.', 'utf8'))
+                return ''
             else:
-                self.wfile.write(bytes(str(data), 'utf8'))
+                self.server.set_state(state[0])
                 self.server.set_code(code[0])
+                self.wfile.write(bytes('Authorised. You can close this browser window, now.', 'utf8'))
 
-    def __init__(self,):
+    def __init__(self):
         super().__init__(server_address=('127.0.0.1',0), RequestHandlerClass=self.Handler)
         self._code = ''
+        self._state = ''
         self._error = ''
-        self._shutdown = False
 
     def get_port(self):
         return self.server_port
 
     def get_code(self):
         return self._code
-
+    
     def set_code(self, code):
         self._code = code
+
+    def get_state(self):
+        return self._state
+
+    def set_state(self, state):
+        self._state = state
 
     def get_error(self):
         return self._error
 
-    def set_error(self, error):
-        self._error = error
-
-    def start(self):
+    def wait_for_authorisation_code(self, timeout=20):
+        self.timeout = timeout
         with self:
-            while self._shutdown == False:
-                self.handle_request()
-                sleep(1)
+            self.handle_request()
 
-    def stop(self):
-        self._shutdown = True
+    def handle_timeout(self):
+        print("error: timeout while waiting for microsoft authorisation code.")
+        self._error = f'timeout after {self.timeout} seconds.'
+        return ''
 
 
