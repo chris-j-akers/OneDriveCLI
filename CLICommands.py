fa6ed6e1fdd9c46b0a18b3c7fec89d6e293f98ba
Chris Akers <chris.j.akers@gmail.com>
2024-04-06 11:24:48 +0100

    formal implementation of commands

--- CLICommands.py
+++ CLICommands.py
@@ -14,7 +14,57 @@ class CLICommand:
         raise NotImplementedError
 
 # Commands 
-    
+
+
+class Init(CLICommand):
+
+    def execute(self):
+        odc = OneDriveCLI()
+        odc.initialise()
+        print('OneDrive CLI for Linux initialised')
+
+
+class IsInit(CLICommand):
+
+    def execute(self):
+        odc = OneDriveCLI()
+        return odc.is_initialised()
+
+class DebugOn(CLICommand):
+
+    def execute(self):
+        odc = OneDriveCLI()
+        odc.debug_on(True)
+
+class DebugOff(CLICommand):
+
+    def execute(self):
+        odc = OneDriveCLI()
+        odc.debug_on(False)
+
+class Pwd(CLICommand):
+
+    def execute(self):
+        odc = OneDriveCLI()
+        print(odc.pwd())
+
+class Ls(CLICommand):
+
+    def execute(self):
+        odc = OneDriveCLI()
+        print(odc.ls())
+
+
+class Cd(CLICommand):
+
+    def __init__(self, arglist) -> None:
+        self._path = self._get_arg(arglist, 2, '/')
+
+    def execute(self):
+        odc = OneDriveCLI()
+        print(odc.cd(self._path))
+
+
 class Get(CLICommand):
 
     def __init__(self, arglist) -> None:
@@ -27,3 +77,18 @@ class Get(CLICommand):
     def execute(self):
         odc = OneDriveCLI()
         odc.get(self._source, self._destination)
+
+
+class Put(CLICommand):
+
+    def __init__(self, arglist) -> None:
+        self.source = self.get_arg(arglist, 2)
+        if self._source is None:
+            print("error: no source file specified")
+            raise ValueError
+        self._destination = self._get_arg(arglist, 3, './')
+
+    def execute(self):
+        odc = OneDriveCLI()
+        odc.put(self.source, self._destination)
+
